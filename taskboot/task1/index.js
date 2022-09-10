ec0dd4c85a331da98c3d69e7605ece4e9ed04396
shrutibutte <83746949+shrutibutte@users.noreply.github.com>
2022-09-10 15:41:29 +0530

    Add files via upload

---
+++ taskboot/task1/index.js
@@ -0,0 +1,26 @@
+const inputText = document.querySelector("#txt");
+const myButton = document.querySelector(".btn-list");
+const list = document.querySelector(".container ul");
+myButton.addEventListener("click", (e) => {
+  if (inputText.value != "") {
+    e.preventDefault();
+    //
+    const myLi = document.createElement("li");
+    myLi.innerHTML = inputText.value;
+    list.appendChild(myLi);
+
+    const mySpan = document.createElement("span");
+    mySpan.innerHTML = "x";
+    myLi.appendChild(mySpan);
+  }
+  const close = document.querySelectorAll("span");
+  for (let i = 0; i < close.length; i++) {
+    close[i].addEventListener("click", () => {
+      close[i].parentElement.style.opacity = 0;
+      setTimeout(() => {
+        close[i].parentElement.style.display = "none";
+      }, 500);
+    });
+  }
+  inputText.value = "";
+});
